cd720e2f4e272098fc43a1917fe10d75d89995a4
PraDieGo96 <pradiego9687@gmail.com>
2025-12-15 20:29:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,45 @@
-//your JS code here. If required.
+const codes = document.querySelectorAll('.code');
+
+codes[0].focus();
+
+
+const codeContainer = document.querySelector('.code-container');
+codeContainer.addEventListener('input', (e) => handleInput(e));
+codeContainer.addEventListener('keydown', (e) => handleKeyDown(e));
+
+
+function handleInput(e) {
+    const input = e.target;
+
+    if (!input.classList.contains('code')) return;
+    const index = parseInt(input.dataset.index);
+    if (input.value.length > 1) {
+        input.value = input.value.slice(0, 1);
+    }
+
+    if (input.value !== '' && index < codes.length - 1) {
+        codes[index + 1].focus();
+    }
+}
+
+function handleKeyDown(e) {
+    const input = e.target;
+    
+    if (!input.classList.contains('code')) return;
+
+    const index = parseInt(input.dataset.index);
+
+    if (e.key === 'Backspace') {
+        e.preventDefault(); 
+        
+        if (input.value !== '') {
+            input.value = '';
+        } 
+        
+        else if (index > 0) {
+            codes[index - 1].focus();
+            codes[index - 1].value = '';
+        }
+        
+    }
+}
